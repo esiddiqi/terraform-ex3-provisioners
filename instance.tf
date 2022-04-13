27d0af92c9fb1ddf834e34816bdeb9e7d583daa3
esiddiqi <siddiquiemaad@gmail.com>
2022-04-13 15:13:16 +0530

    Created resources

--- instance.tf
+++ instance.tf
@@ -24,7 +24,7 @@ resource "aws_instance" "terra-instance" {
 
  provisioner remote-exec{
    inline = [
-       "chmod u+x /tmp/tmp.sh"
+       "chmod u+x /tmp/tmp.sh",
        "sudo /tmp/tmp.sh"
 
    ]
